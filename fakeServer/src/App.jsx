b65300a6aa84d2fcb036646b81b40ffc39a406af
Rivka Sorscher & Leah Shitrit <rsorscher@outlook.com>
2024-01-12 01:22:59 +0200

    jjj

--- fakeServer/src/App.jsx
+++ fakeServer/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router,Route ,Routes} from 'react-router-dom';
+import {BrowserRouter as Router,Route ,Routes, Navigate} from 'react-router-dom';
 
 import Home from './components/Home';
 import Login from './components/Login';
@@ -11,6 +11,7 @@ function App() {
     <>
       <Router>
         <Routes >
+          <Route path='/' element={<Navigate to={'/login'}/>} />
           <Route path='/home'element={<Home/>}/>
           <Route path='/login'element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
