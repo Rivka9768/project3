1d8b2a957b63931bd0bebb88767d7b8654174f9e
Rivka&Leah <rl@gmail.com>
2024-01-14 14:38:17 +0200

    hfjfgj

--- fakeServer/src/components/Login.jsx
+++ fakeServer/src/components/Login.jsx
@@ -4,59 +4,45 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
-    const [error, setError] = useState({ name: "", password: "" });
-    const [input, setinput] = useState({ name: "", password: "" });
     const [exist, setExist] = useState(true);
     const navigate = useNavigate();
     debugger;
     const isExist = (name, password) => {
-        fetch(`http://localhost:3000/users?name=${name}&&website=${password}`)
+        fetch(`http://localhost:3000/users?username=${name}&&website=${password}`)//לא עשינו לןכל סטורג
             .then(response => response.json())
-            .then( response =>  (response.length)? navigate('/home'):setExist(false) )
+            .then(response => (response.length) ? navigate('/home') : setExist(false))
     }
 
     const logIn = (event) => {
         event.preventDefault();
         const [name, password] = event.target;
-        debugger
-        let tempError = { name: "", password: "" };
-        if (!name.value)
-            tempError = { ...tempError, name: "please enter a name" }
-        if (!password.value)
-            tempError = { ...tempError, password: "please enter a password" }
-        setError(tempError);
+       debugger 
+        if (!validateInput(name.value, password.value)) {
+            setExist(false);
+            return
+        }
+        setExist(true);
         isExist(name.value, password.value)
     }
-    const validateInput = (event) => {
-        event.preventDefault();
-        const { name, value } = event.target;
-        switch (name) {
-            case "name": {
-                if (/^[a-zA-Z ]+$/.test(value) === false)
-                    setError({ ...error, name: "name illegal" })
-                else
-                    setError({ ...error, name: "" })
-                break;
-            }
-            case "password": {
-                if ((/^[a-zA-Z.]+$/.test(value) === false) || value.indexOf('.') === -1)
-                    setError({ ...error, password: "password illegal" })
-                else
-                    setError({ ...error, password: "" })
-            }
-        }
+
+    const validateInput = (name, password) => {
+        if (/^[a-zA-Z ]+$/.test(name) === false)//האם לאפשר שגם מספרים ותווים
+            return false;
+        if ((/^[a-zA-Z.]+$/.test(password) === false) || password.indexOf('.') === -1)
+            return false;
+        if (!name || !password)
+            return false;
+        return true;
     }
 
     return (
         <>
             <h1>login</h1>
-            {!exist&&<div>Incorrect username or password</div>}
+            {!exist && <div>Incorrect username or password</div>}
             <form noValidate onSubmit={logIn}>
-                <input type='text' name='name' placeholder='name' onChange={validateInput}></input>
-                {error.name ? <p>{error.name}</p> : <br />}
-                <input type="password" name="password" id="" placeholder='password' onChange={validateInput} />
-                {error.password ? <p>{error.password}</p> : <br />}
-                <input disabled={error.name ? true : false} type="submit" value="Log In" />
+                <input type='text' name='name' placeholder='name' ></input>
+                <input type="password" name="password" id="" placeholder='password' />
+                <input type="submit" value="Log In" />
             </form>
         </>
     )
--- fakeServer/src/components/Register.jsx
+++ fakeServer/src/components/Register.jsx
@@ -3,8 +3,70 @@ import  React from 'react'
 const Register=()=>{
 return(
     <>
-    <h1>register</h1>
+     <form noValidate onSubmit={logIn}>
+                 <input type='text' name='name' placeholder='name' onChange={validateInput}></input>
+                 <input type="password" name="password" id="" placeholder='password' onChange={validateInput} />
+                 <input type="password" name="passwordVerify" id="" placeholder='password verification' onChange={validateInput} />
+                 <input type="submit" value="Sign Up" />
+           </form>
     </>
 )
 }
 export default Register
+
+
+// const Login = () => {
+//     const [error, setError] = useState({ name: "", password: "" });
+//     const [exist, setExist] = useState(true);
+//     const navigate = useNavigate();
+//     debugger;
+//     const isExist = (name, password) => {
+//         fetch(`http://localhost:3000/users?name=${name}&&website=${password}`)
+//             .then(response => response.json())
+//             .then(response => (response.length) ? navigate('/home') : setExist(false))
+//     }
+
+//     const logIn = (event) => {
+//         event.preventDefault();
+//         const [name, password] = event.target;
+//         if (!name.value || !password.value) {
+//             setExist(false);
+//             return
+//         }
+//         isExist(name.value, password.value)
+//     }
+//     const validateInput = (event) => {
+//         event.preventDefault();
+//         const { name, value } = event.target;
+//         switch (name) {
+//             case "name": {
+//                 if (/^[a-zA-Z ]+$/.test(value) === false)
+//                     setError({ ...error, name: "name illegal" })
+//                 else
+//                     setError({ ...error, name: "" })
+//                 break;
+//             }
+//             case "password": {
+//                 if ((/^[a-zA-Z.]+$/.test(value) === false) || value.indexOf('.') === -1)
+//                     setError({ ...error, password: "password illegal" })
+//                 else
+//                     setError({ ...error, password: "" })
+//             }
+//         }
+//     }
+
+//     return (
+//         <>
+//             <h1>login</h1>
+//             {!exist && <div>Incorrect username or password</div>}
+//             <form noValidate onSubmit={logIn}>
+//                 <input type='text' name='name' placeholder='name' onChange={validateInput}></input>
+//                 {error.name ? <p>{error.name}</p> : <br />}
+//                 <input type="password" name="password" id="" placeholder='password' onChange={validateInput} />
+//                 {error.password ? <p>{error.password}</p> : <br />}
+//                 <input disabled={error.name || error.password ? true : false} type="submit" value="Log In" />
+//             </form>
+//         </>
+//     )
+// }
+// export default Login
