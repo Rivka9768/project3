aba49e43618bf1848df9f4c106da11b8cea117b0
Rivka&Leah <rl@gmail.com>
2024-01-17 21:33:00 +0200

    eqyt

--- fakeServer/src/components/posts/Posts.jsx
+++ fakeServer/src/components/posts/Posts.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from "react";
+import { MdDelete, MdModeEdit } from "react-icons/md";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { FaEyeSlash, FaEye } from "react-icons/fa";
 import SearchPosts from "./SearchPosts";
@@ -9,6 +10,7 @@ const Posts = () => {
   const [posts, setPosts] = useState([]);
   const [showBody, setShowBody] = useState(-1)
   let [allPosts, setAllPosts] = useState([])
+  const [isUpdate, setIsUpdate] = useState(-1);
   const getPosts = () => {
     fetch(`http://localhost:3000/posts?userId=${user.id}`)
       .then(async response => {
@@ -25,17 +27,20 @@ const Posts = () => {
   return (
     <>
       <h1>Posts</h1>
-      <SearchPosts/>
+      <SearchPosts setPosts={setPosts} allPosts={allPosts} posts={posts}/>
       {!exist ? <AiOutlineLoading3Quarters /> : < >
         {posts.map((post, index) =>
           <div style={{ fontWeight: (showBody === index) && 'bold' }} key={index}>
             <span>ID: {post.id}</span>
             <span>TITLE: {post.title}</span>
-            {showBody === index && <span>BODY: {post.body}</span>}
+            {showBody === index && <span>BODY: {post.body}</span>
+                        && <button onClick={() => setIsUpdate(prevIsUpdate => prevIsUpdate === -1 ? index : -1)}><MdModeEdit /></button>
+                        
+            }
             <button onClick={() => setShowBody(prevShowBody => prevShowBody === index ? -1 : index)}> {showBody === index ? <FaEyeSlash /> : <FaEye />}</button>
 
-            {/* <button onClick={() => setIsUpdate(prevIsUpdate => prevIsUpdate === -1 ? index : -1)}><MdModeEdit /></button>
-            <button disabled={isUpdate === index} onClick={() => remove(todo.id)}><MdDelete /></button> */}
+
+            <button disabled={isUpdate === index} onClick={() => remove(todo.id)}><MdDelete /></button> 
           </div>
         )}</>}
     </>
--- fakeServer/src/components/posts/SearchPosts.jsx
+++ fakeServer/src/components/posts/SearchPosts.jsx
@@ -2,12 +2,42 @@ import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import makeAnimated from 'react-select/animated'
 
-const SearchPosts = () => {
+const SearchPosts = ({setPosts,allPosts,posts}) => {
     const animatecomponent = makeAnimated();
     const search = [{ label: "id", value: "id" }, { label: "title", value: "title" }];
-    const selectOption=()=>{
-        
+    const [searchValues, setSearchValues] = useState([])
+    const selectOption = (e) => {
+        setSearchValues(e)
+        console.log(e.length)
+        e.length === 0 && setPosts(allPosts)
     }
+
+
+    const searchByOption = (element) => {
+        element.preventDefault();
+        let values = [{ type: "id", value: "" }, { type: "title", value: "" }];
+        for (let i = 0; i < element.target.length - 1; i++) {
+          switch (element.target[i].name) {
+            case "id":
+              values[0] = { ...values[0], value: element.target[i].value }
+              break;
+            case "title":
+              values[1] = { ...values[1], value: element.target[i].value }
+              break;
+          }
+        }
+        element.target.reset()
+        let tempSelectedPosts = [];
+        posts.forEach((post) => {
+          if (((values[0].value != "") ? post.id === values[0].value : true)
+            && ((values[1].value != "") ? post.title === values[1].value : true)
+          ) 
+            tempSelectedPosts.push(post)
+        })
+        console.log(tempSelectedPosts)
+        setPosts(tempSelectedPosts)
+      }
+    
     return (
         <>
             <Select
@@ -18,6 +48,7 @@ const SearchPosts = () => {
                 isMulti
                 getOptionLabel={(search) => search["label"]}
                 getOptionValue={(search) => search["value"]} />
+            {searchValues.length ? <form onSubmit={searchByOption}>{searchValues.map((value, index) => <input key={index} name={value.label} placeholder={value.label} />)}<input type="submit" /></form> : null}
         </>
     )
 }
